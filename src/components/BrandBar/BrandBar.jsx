70a286cc791b3c90e40cc1afe95494f372b39d03
Bohdan <freelancer.kkapkko@gmail.com>
2024-11-27 00:54:14 +0200

    add deploy workflow

--- src/components/BrandBar/BrandBar.jsx
+++ src/components/BrandBar/BrandBar.jsx
@@ -7,11 +7,11 @@ import {
 } from "../../motion-setting";
 
 const logoBrands = [
-	{ id: 1, link: "./src/assets/images/google.svg", alt: "Google" },
-	{ id: 2, link: "./src/assets/images/nike.svg", alt: "Nike" },
-	{ id: 3, link: "./src/assets/images/samsung.svg", alt: "Samsung" },
-	{ id: 4, link: "./src/assets/images/apple.svg", alt: "Apple" },
-	{ id: 5, link: "./src/assets/images/adidas.svg", alt: "Adidas" },
+	{ id: 1, link: "./assets/images/google.svg", alt: "Google" },
+	{ id: 2, link: "./assets/images/nike.svg", alt: "Nike" },
+	{ id: 3, link: "./assets/images/samsung.svg", alt: "Samsung" },
+	{ id: 4, link: "./assets/images/apple.svg", alt: "Apple" },
+	{ id: 5, link: "./assets/images/adidas.svg", alt: "Adidas" },
 ];
 
 const BrandBar = () => {
